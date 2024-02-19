ac4b1069ebc7b4c4496ecdfd26c9047a1e5582f1
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-19 00:21:26 -0500

    first commit

---
+++ LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Teodoro Calle Lara on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
