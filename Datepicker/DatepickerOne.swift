43513646751dacb6f065a17c4746d590e7d7ec1b
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-19 10:26:49 -0500

    add DatePicker

---
+++ Datepicker/DatepickerOne.swift
@@ -0,0 +1,18 @@
+//
+//  DatepickerOne.swift
+//  LearningSwiftUI
+//
+//  Created by Teodoro Calle Lara on 20/02/24.
+//
+
+import SwiftUI
+
+struct DatepickerOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatepickerOne()
+}
---
+++ Datepicker/DatepickerTwo.swift
@@ -0,0 +1,18 @@
+//
+//  DatepickerTwo.swift
+//  LearningSwiftUI
+//
+//  Created by Teodoro Calle Lara on 20/02/24.
+//
+
+import SwiftUI
+
+struct DatepickerTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatepickerTwo()
+}
