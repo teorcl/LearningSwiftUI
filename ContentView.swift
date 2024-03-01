b60e92043327873279aa3200e52c810eaec9992f
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-03-01 09:08:27 -0500

    Se implementa List básico

--- ContentView.swift
+++ ContentView.swift
@@ -38,6 +38,8 @@ struct ContentView: View {
             //LazygridTwo()
             //LazygridThree()
             //LazygridFour()
+            //FormOne()
+            ListOne()
             
         }
 //        .onTapGesture {
